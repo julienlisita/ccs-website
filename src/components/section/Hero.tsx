d43186822e425b92efcf44713f0297fbfafd19b9
Julien Lisita <julien.lisita@gmail.com>
2025-09-30 20:24:04 +0200

    style(ui): apply background image and colors to Hero section

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -4,20 +4,32 @@
 
 import Button from '@/components/ui/Button';
 import PageTitle from '@/components/ui/PageTitle';
+// import './Hero.css';
 
 export default function Hero() {
   return (
     <section
-      className="w-full  flex flex-col justify-center  text-[#3B2F2F] px-4 text-center"
+      className="hero w-full  flex flex-col justify-center  text-[#3B2F2F] px-4 text-center"
       style={{ minHeight: 'calc(100vh - var(--header-height))' }}
     >
-      <div className="max-w-4xl mx-auto space-y-6">
-        <PageTitle> Mettez en valeur votre activité avec un site professionnel</PageTitle>
-        <p className="text-lg sm:text-xl text-[#3B2F2F]/80">
-          Un design clair, responsive et rapide à mettre en place.
+      {/* Image de fond */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/hero.avif') " }}
+      />
+      {/* Overlay noir avec opacité */}
+      <div className="absolute inset-0 bg-black/60" />
+
+      <div className="max-w-4xl mx-auto space-y-6 z-10">
+        <PageTitle color="#FFFFFF"> Company Care & Services</PageTitle>
+        <h2 className="text-xl sm:text-2xl lg:text-3xl text-white">
+          Un accompagnement humain et professionnel à domicile
+        </h2>
+        <p className="text-sm sm:text-lg text-white/80">
+          Un accompagnement humain et professionnel à domicile
         </p>
         <Button variant="primary" href="/contact">
-          Demander un devis
+          Nous contacter
         </Button>
       </div>
     </section>
